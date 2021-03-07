27f0f7e656f2210e5841078f2455c7199c158b47
Bruno Becoski <brunobecoski@gmail.com>
2021-03-06 23:53:45 -0300

    Update Login css

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -14,33 +14,48 @@ export function Login() {
   }
 
   return (
+    <div className={styles.container}>
 
-    <main className={styles.loginContainer}>
       <img src="/icons/simbolo.svg" alt="Move It Símbolo" />
 
-      <div className={styles.loginForm}>
+      <main className={styles.main}>
+
         <img src="/icons/logo-full.svg" alt="Move It Logo" />
+
         <h1>Bem-vindo</h1>
 
-        <p>Crie sua conta agora mesmo.</p>
+        <p>Crie sua conta agora mesmo</p>
 
-        <form>
+        <form className={styles.form}>
+
           <label htmlFor="name">
-            <input id="name" placeholder="Digite seu nome"
+
+            <input
+              id="name"
+              placeholder="Digite seu nome"
               onChange={event => (handleInputName(event.target.value))}
             />
+
             <button type="submit" onClick={handleSubmit}>
+
               <img src="/icons/arrow-right.svg" />
+
             </button>
+
           </label>
+
         </form>
 
         <button className={styles.github}>
+
           <img src="/icons/github.svg" alt="GitHub" />
+
           <p>Use seu perfil do Github para começar</p>
+
         </button>
 
-      </div>
-    </main>
+      </main>
+
+    </div>
   );
 }
